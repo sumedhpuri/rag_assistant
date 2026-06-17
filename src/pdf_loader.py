0132a27ccee79eab64dc67cfb571adc340285cbf
Sumedh <146160339+sumedhpuri@users.noreply.github.com>
2026-06-17 22:05:05 +0530

    read pdfs

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -1,2 +1,23 @@
-import os
-os.environ('pip install')
+from pypdf import PdfReader
+
+
+def pdf_loader():
+    page_counter = 0
+    total_words = 0
+    words_per_page = []
+    average_words_per_page = 0
+    max_words_per_page = 0
+
+    for i,j in enumerate(reader.pages):
+        current_page = j.extract_text()
+        
+        total_words_in_page = current_page.split()
+        
+        if max_words_per_page < len(total_words_in_page):
+            max_words_per_page = len(total_words_in_page)
+        
+        words_per_page.append(len(total_words_in_page))
+
+    print('max words in a page :',max_words_per_page) 
+    print('total words in the PDF :',sum(words_per_page))
+    print('average number of words per page: ', round(sum(words_per_page)/len(words_per_page),0))
