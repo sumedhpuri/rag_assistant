c25007b48e5d62eff57d76055553ef993fc32d3f
Sumedh <146160339+sumedhpuri@users.noreply.github.com>
2026-06-18 17:09:10 +0530

    chunker initiation

--- src/pdf_loader.py
+++ src/pdf_loader.py
@@ -1,26 +1,37 @@
 from pypdf import PdfReader
 
-folder_path = r"C:\Users\Sumed\Desktop\rag_assistant\data"
+folder_path = r"C:\Users\Sumed\Desktop\rag_assistant\data\*pdf"
 
 def pdf_loader(folder_path):
+    pdf_files = glob.glob(folder_path)
+
+    if not pdf_files:
+            raise Exception('No document(s) found')
+    else:
+        print(f'{len(pdf_files)} document(s) found')
+
+    for i in pdf_files:
+        print(i)
     
-    reader = PdfReader(folder_path + '\Mathematics of Deep Learning.pdf')
     page_counter = 0
     total_words = 0
     words_per_page = []
     average_words_per_page = 0
-    max_words_per_page = 0
 
-    for i,j in enumerate(reader.pages):
-        current_page = j.extract_text()
-        
-        total_words_in_page = current_page.split()
-        
-        if max_words_per_page < len(total_words_in_page):
-            max_words_per_page = len(total_words_in_page)
-        
-        words_per_page.append(len(total_words_in_page))
+    for i in pdf_files:
+        print('processing file : ', i)
+        reader = PdfReader(i)
+        max_words_per_page = 0
 
-    print('max words in a page :',max_words_per_page) 
-    print('total words in the PDF :',sum(words_per_page))
-    print('average number of words per page: ', round(sum(words_per_page)/len(words_per_page),0))
+        for _,j in enumerate(reader.pages):
+            current_page = j.extract_text()
+            total_words_in_page = current_page.split()
+            
+            if max_words_per_page < len(total_words_in_page):
+                max_words_per_page = len(total_words_in_page)
+            words_per_page.append(len(total_words_in_page))
+
+        print('max words in a page :',max_words_per_page) 
+        print('total words in the PDF :',sum(words_per_page))
+        print('average number of words per page: ', round(np.divide( sum(words_per_page),len(words_per_page) ),0) )
+        print('\n')
